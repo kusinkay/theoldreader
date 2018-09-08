8668f8872a6d013cb8449dae28ad5b85b9112ddd
Juane Puig <juanepm@gmail.com>
2018-09-08 10:22:08 +0200

    Pagination and mode filter
    Some features added:
    - Allow posts pagination 
    - Match for audio enctype and filter subscriptions

--- theoldreader/tor.py
+++ theoldreader/tor.py
@@ -44,7 +44,8 @@ class Connection(object):
 
         var_json = {'output': 'json'}
         var_json.update(var)
-
+        for param in var:
+            self._logger.debug(param + ":" + str(var[param]))
         if use_get:
             response = requests.get(url, params=var_json, headers=header)
         else:
@@ -103,6 +104,9 @@ class Item(object):
         self.title = None
         self.content = None
         self.href = None
+        self.mediaUrl = None
+        self.published = None
+        self.enctype = None
 
     # TODO: No use_get
     def _make_api_request(self, url_end, var, use_get=True):
@@ -181,11 +185,15 @@ class Item(object):
         item_det = resp3['items'][0]
         self.title = item_det['title']
         self.content = item_det['summary']['content']
+        self.published = item_det['published']
         self.href = item_det['alternate'][0]['href']
-
+        if 'enclosure' in item_det:
+            self.mediaUrl = item_det['enclosure'][0]['href']
+            self.enctype = item_det['enclosure'][0]['type']
+        self.source = resp3['title']
+        self.source_id = resp3['id']
 
 class ItemsSearch(object):
-
     def __init__(self, connection):
         """
         Initialize object
@@ -194,6 +202,7 @@ class ItemsSearch(object):
         :rtype: None
         """
         self.connection = connection
+        self.next_pointer = None
 
     def _make_search_request(self, var, limit_items=1000):
         var['n'] = limit_items
@@ -203,52 +212,223 @@ class ItemsSearch(object):
             use_get=True
         )
 
-    def _load_rest(self, continuation, var, limit_items=1000, items_list=None):
+    def _load_rest(self, continuation, var, limit_items=1000, items_list=None, stop=False):
         if items_list is None:
             items_list = []
         while continuation is not None:
             var['c'] = continuation
             resp = self._make_search_request(var, limit_items)
             continuation = resp.get('continuation')
+            self.next_pointer = continuation
             items_list.extend(resp['itemRefs'])
+            if stop:
+                break
         return [
             Item(self.connection, item.get('id'))
             for item in items_list
         ]
+        
+    def _pre_load_rest(self, continuation, var, limit_items=1000, stop=False):
+        ''' 
+        private function to prevent repeating same code for diferent request modes
+        :param continuation: id for the next bloc (limit_items-sized) of feeds
+        :param var: parameters in the TOR request
+        :param limit_items: size of the feeds bloc
+        :param stop: stops asking for further blocs
+        '''
+        if continuation!=None and stop==True:
+            # asking for a new page
+            items_list = None
+        else:
+            #asking for the first page or all of them
+            resp = self._make_search_request(var, limit_items)
+            continuation = resp.get('continuation')
+            self.next_pointer = continuation
+            items_list = resp.get('itemRefs', [])
+            if (stop):
+                #just the first one, this will prevent loop inside _load_rest
+                continuation = None
+        return self._load_rest(continuation, var, limit_items, items_list, stop)
 
-    def get_unread_only(self, limit_items=1000):
+    def get_unread_only(self, limit_items=1000, feed=None, stop=False, continuation=None):
+        ''' 
+        Gets just unread posts
+        :param limit_items: size of the feeds bloc
+        :param feed: gets only posts from this feed (all if none)
+        :param stop: stops asking for further blocs
+        :param continuation: id for the next bloc (limit_items-sized) of feeds
+        '''
+        
         var = {
             's': 'user/-/state/com.google/reading-list',
             'xt': 'user/-/state/com.google/read'
         }
-        resp = self._make_search_request(var, limit_items)
-        continuation = resp.get('continuation')
-        items_list = resp.get('itemRefs', [])
-        return self._load_rest(continuation, var, limit_items, items_list)
+        if (feed!=None):
+            var['s'] = feed
+        return self._pre_load_rest(continuation, var, limit_items, stop)
 
-    def get_starred_only(self, limit_items=1000):
+    def get_starred_only(self, limit_items=1000, feed=None, stop=False, continuation=None):
+        ''' 
+        Gets just starred posts
+        :param limit_items: size of the feeds bloc
+        :param feed: gets only posts from this feed (all if none)
+        :param stop: stops asking for further blocs
+        :param continuation: id for the next bloc (limit_items-sized) of feeds
+        '''
         var = {
             's': 'user/-/state/com.google/starred'
         }
-        resp = self._make_search_request(var, limit_items)
-        continuation = resp.get('continuation')
-        items_list = resp.get('itemRefs', [])
-        return self._load_rest(continuation, var, limit_items, items_list)
+        if (feed!=None):
+            var['s'] = feed
+        return self._pre_load_rest(continuation, var, limit_items, stop)
 
-    def get_liked_only(self, limit_items=1000):
+    def get_liked_only(self, limit_items=1000, feed=None, stop=False, continuation=None):
+        ''' 
+        Gets just liked posts
+        :param limit_items: size of the feeds bloc
+        :param feed: gets only posts from this feed (all if none)
+        :param stop: stops asking for further blocs
+        :param continuation: id for the next bloc (limit_items-sized) of feeds
+        '''
         var = {
             's': 'user/-/state/com.google/like'
         }
-        resp = self._make_search_request(var, limit_items)
-        continuation = resp.get('continuation')
-        items_list = resp.get('itemRefs', [])
-        return self._load_rest(continuation, var, limit_items, items_list)
+        if (feed!=None):
+            var['s'] = feed
+        return self._pre_load_rest(continuation, var, limit_items, stop)
 
-    def get_shared_only(self, limit_items=1000):
+    def get_shared_only(self, limit_items=1000, feed=None, stop=False, continuation=None):
+        ''' 
+        Gets just shared posts
+        :param limit_items: size of the feeds bloc
+        :param feed: gets only posts from this feed (all if none)
+        :param stop: stops asking for further blocs
+        :param continuation: id for the next bloc (limit_items-sized) of feeds
+        '''
         var = {
             's': 'user/-/state/com.google/broadcast'
         }
-        resp = self._make_search_request(var, limit_items)
-        continuation = resp.get('continuation')
-        items_list = resp.get('itemRefs', [])
-        return self._load_rest(continuation, var, limit_items, items_list)
+        if (feed!=None):
+            var['s'] = feed
+        return self._pre_load_rest(continuation, var, limit_items, stop)
+
+class Subscriptions (object):
+    def __init__(self, connection, mode = None):
+        """
+        Initialize object
+        :param connection: The corresponding connection
+        :type connection: TheOldReaderConnection
+        :rtype: None
+        
+        :param mode: search the beginning of the post enctype (i.e. 'audio')
+        """
+        self.connection = connection
+        self.id = None
+        self.title = None
+        self.iconUrl = None
+        self.firstitemmsec = None
+        self.unread_count = 0
+        
+        self.unread_response = None
+        self.mode = mode
+        self.matches = False
+        
+    def get_all(self, limit_items=1000):
+        return self.get_items(False, limit_items)
+    
+    def get_unread(self, limit_items=1000):
+        return self.get_items(True, limit_items)
+    
+    def get_all_cursor(self, limit_items=1000):
+        return SubscriptionsCursor(self.get_items(False, limit_items), self.mode)
+    
+    def get_unread_cursor(self, limit_items=1000):
+        return SubscriptionsCursor(self.get_items(True, limit_items), self.mode)
+        
+    def get_items(self, just_unread = False, limit_items=1000):
+        var = {
+            
+        }
+        var['n'] = limit_items
+        response = self.connection.make_request(
+            url_api + 'subscription/list',
+            var,
+            use_get=True
+        )
+        
+        if just_unread:
+            self.unread_response = self.connection.make_request(
+                url_api + 'unread-count',
+                var,
+                use_get=True
+            )
+        
+        s_list = []
+        for feed in response['subscriptions']:
+            s = Subscriptions(self.connection)
+            s.id =  feed['id']
+            s.title = feed['title']
+            s.iconUrl = 'http:' + feed['iconUrl']
+            s.firstitemmsec = feed['firstitemmsec']
+            s.unread_count = self.count_unread(feed['id'])
+            s.mode = self.mode
+            
+            if (just_unread==False or s.unread_count>0):
+                s_list.append(s)
+            
+        return s_list
+    
+    def count_unread(self, feedId):
+        '''
+        Once list is gotten, helps to find the feed unread counts
+        '''
+        for counter in self.unread_response['unreadcounts']:
+            if counter['id']==feedId:
+                return counter['count']
+            
+        return 0
+    
+    def matches_mode (self, feedId):
+        '''
+        Checks if the first post of the feed matches with the mode
+        Better use it with a SubscriptionsCursor and informing about the progress, cause it's a quite expensive process
+        '''
+        if self.mode != None:
+            search = ItemsSearch(self.connection)
+            unread = search.get_unread_only(1, feedId, True)
+            item = unread[0]
+            if item != None:
+                item.get_details()
+                if item.enctype != None and item.enctype.startswith(self.mode):
+                    return True
+                else:
+                    return False
+        else:
+            return True
+
+
+class SubscriptionsCursor(object):
+    '''
+    maintains an array and provides deferred, expensive queries to be done one by one
+    '''
+    def __init__(self, subscriptions, mode):
+        self.subscriptions = subscriptions
+        self.mode = mode
+        self.current = 0
+        
+    def next(self):
+        if self.current>=len(self.subscriptions):
+            return False
+        s = self.subscriptions[self.current]
+        self.current = self.current + 1
+        if s.matches_mode(s.id):
+            s.matches = True
+        return s
+    
+    def reset(self):
+        self.current = 0
+    
+        
+        
+        
+        
